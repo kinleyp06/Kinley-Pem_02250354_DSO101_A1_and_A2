56ddc50ef5cc8e2f1d27838105fef1fa99751708
kinleyp06 <kinleypem06@gmail.com>
2026-05-14 00:12:37 +0600

    final

--- KinleyPem_02250354_DSO101_A1_and_A2/backend/db.js
+++ KinleyPem_02250354_DSO101_A1_and_A2/backend/db.js
@@ -3,9 +3,20 @@ require("dotenv").config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? {
+          rejectUnauthorized: false,
+        }
+      : false,
+});
+
+pool.connect((err) => {
+  if (err) {
+    console.error("Database connection error:", err);
+  } else {
+    console.log("Database connected successfully");
+  }
 });
 
 module.exports = pool;
